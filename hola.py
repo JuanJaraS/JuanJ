4d79d64c793420f3f1ff49693f6b9bab6eab419c
JuanJaraS <juan.jarasaldias@gmail.com>
2025-04-16 16:40:55 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,62 @@
+# nombre="Juan"
+# edad="23"
+# print("Hola",nombre,"tu edad es",edad)
+# solicitud de datos
+
+# print("Ingrese su nombre")
+# nombre=input()
+# print("Ingrese su edad")
+# edad=input()
+# print("Hola",nombre,"tu edad es",edad)
+
+# Caracter=string
+# Entero=integer
+# Real=float
+# Logico=boolean(t,f)(verdadero,falso)
+
+# print("Ingrese el primer número para sumar")
+# n1=int(input())
+# print("Ingrese el segundo número para sumar")
+# n2=int(input())
+# print("La suma es", n1+n2)
+
+# print("Ingrese 3 números para sacar el promedio")
+# n1=int(input())
+# n2=int(input())
+# n3=int(input())
+# prom=(n1+n2+n3)/3
+# print("El promedio de sus numeros es", prom)
+# if prom>=40:
+#     print("El alumno aprobó")
+# else:
+#     print("El alumno reprobó")
+
+
+# edad=int(input("Ingrese su edad "))
+# if edad>=18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted no es mayor de edad")
+
+# edad=int(input("Ingrese su edad "))
+# if edad<12:
+#     print("usted es un niño")
+# elif edad>=12 and edad<=17:
+#     print("usted es un adolescente")
+# elif edad>=18 and edad<=64:
+#     print("usted es un adulto")
+# else:
+#     print("Usted es un adulto mayor")
+
+print("Ingrese 3 números")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if n1>n2 and n1>n3:
+    print("El numero mayor es",n1)
+elif n2>n1 and n2>n3:
+    print("El numero mayor es",n2)
+else:
+    print("El numero mayor es",n3)
+
+    
